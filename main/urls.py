76d8784c1e96f6fb189acb2e66d4e09393d3d335
Cubong <cubongjeremiah@gmail.com>
2023-07-29 14:20:24 +0300

    Delete Functionality has been added

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register')
+    path('register/', views.register_user, name='register'),
+    path('customer/<int:pk>', views.get_customer, name='customer'),
+    path('delete/<int:pk>', views.delete_customer, name='delete'),
+    path('new/', views.register_customer, name='new_customer'),
+
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from . forms import SignUpForm
+from . models import Customer
 
 # Create your views here.
 
 
 def home(request):
+
+    customers = Customer.objects.all()
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -19,7 +24,7 @@ def home(request):
             messages.success(request, "There was an error logging in ......")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'customers': customers})
 
 
 # def login_user(request):
@@ -33,4 +38,38 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+        # authentocate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have logged in succesfully")
+            return redirect('home')
+    else:
+        form = SignUpForm
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def get_customer(request, pk):
+
+    customer = get_object_or_404(Customer, pk=pk)
+
+    return render(request, 'customer.html', {'customer': customer})
+
+
+@login_required
+def delete_customer(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customer.delete()
+    messages.success(request, "Customer Successfully deleted")
+    return redirect('home')
+
+
+def register_customer(request):
+
+    return render(request, 'new.html', {})
